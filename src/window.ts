98499c8eea43cd529ca7466ed3319a1cbac0ed3b
Leonhard Tissen <contact@warze.org>
2023-11-21 14:34:01 +0100

    Fix 2nd window from causing a fresh start on the 1st

--- src/window.ts
+++ src/window.ts
@@ -24,66 +24,78 @@ export function launchProgram(program: DesktopIcon): false {
 
 	const windowId = (Math.floor(Math.random() * (1e+20))).toString(36);
 	const mobileView = window.innerWidth < 1080;
-	const maximizedWindow = mobileView ? 'maximized' : 'programAnimation';
 	const zoomPrc = `${100 / program.zoom}%`;
-	windows.innerHTML += /*html*/`
-	<div id="${windowId}" class="rounded-lg bg-slate-800 bg-opacity-40 backdrop-blur ${maximizedWindow} absolute flex flex-col resize pointer-events-auto overflow-hidden"
-		style="width: ${program.width}px; height: ${program.height}px; top: ${windowY}px; left: ${windowX}px; min-width: 300px; min-height: 30px;">
-		<div class="${mobileView ? 'h-16' : 'h-8'} flex justify-between flex-row">
-			<div class="grow flex flex-row items-center" onpointerdown="document.getElementById('${windowId}').classList.add('dragging');">
-				<img src="/icons/${program.imagedata}" class="h-6 w-6 ml-1">
-				<p class="p-1 select-none">${program.windowname}</p>
-			</div>
-			<div class="flex justify-center items-stretch h-full">
-				<!-- Reload iframe -->
-				<div class="hover:bg-white hover:bg-opacity-10 cursor-pointer flex items-center"
-					onclick="document.getElementById('${windowId}iframe').src = document.getElementById('${windowId}iframe').src"
-				>
-					<svg width="50" height="20">
-						<path d="M20,5 18,5 18,15 32,15 32,5 25,5 27,3 27,7 25,5" fill="none" stroke="white" stroke-linejoin="round" stroke-linecap="round" stroke-width="2"></path>
-					</svg>
-				</div>
-				<!-- Fullscreen -->
-				<div class="hover:bg-white hover:bg-opacity-10 cursor-pointer flex items-center"
-					onclick="document.getElementById('${windowId}iframe').requestFullscreen();"
-				>
-					<svg width="50" height="20">
-						<path d="M29,5 32,5 32,8 M32,12 32,15 29,15 M21,5 18,5 18,8 M18,12 18,15 21,15" fill="none" stroke="white" stroke-linejoin="round" stroke-linecap="round" stroke-width="2"></path>
-					</svg>
-				</div>
-				<!-- Minimize window -->
-				<div class="hover:bg-white hover:bg-opacity-10 cursor-pointer flex items-center"
-					onclick="document.getElementById('${windowId}').classList.add('minimized')";
-				>
-					<svg width="50" height="20">
-						<path d="M20,10 30,10" fill="none" stroke="white" stroke-linejoin="round" stroke-linecap="round" stroke-width="2"></path>
-					</svg>
-				</div>
-				<!-- Maximize window -->
-				<div class="hover:bg-white hover:bg-opacity-10 cursor-pointer flex items-center"
-					onclick="document.getElementById('${windowId}').classList.toggle('maximized')";
-				>
-					<svg width="50" height="20">
-						<path d="M18,5 32,5 32,15 18,15 18,5" fill="none" stroke="white" stroke-linejoin="round" stroke-linecap="round" stroke-width="2"></path>
-					</svg>
-				</div>
-				<!-- Close window -->
-				<div class="hover:bg-red-500 cursor-pointer flex items-center"
-					onclick="document.getElementById('${windowId}').remove();"
-				>
-					<svg width="50" height="20">
-						<path d="M20,5 30,15 M30,5 20,15 25" fill="none" stroke="white" stroke-linejoin="round" stroke-linecap="round" stroke-width="2"></path>
-					</svg>
-				</div>
-			</div>
+
+	const windowElement = document.createElement('div');
+	windowElement.id = windowId;
+	if (mobileView) {
+		windowElement.classList.add('maximized');
+	} else {
+		windowElement.classList.add('programAnimation');
+	}
+	windowElement.classList.add('absolute', 'flex', 'flex-col', 'resize', 'pointer-events-auto', 'overflow-hidden', 'rounded-lg', 'bg-slate-800', 'bg-opacity-40', 'backdrop-blur')
+	windowElement.style.width = `${program.width}px`;
+	windowElement.style.height = `${program.height}px`;
+	windowElement.style.top = `${windowY}px`;
+	windowElement.style.left = `${windowX}px`;
+	windowElement.style.minWidth = '300px';
+	windowElement.style.minHeight = '30px';
+	windowElement.innerHTML = /*html*/`
+	<div class="${mobileView ? 'h-16' : 'h-8'} flex justify-between flex-row">
+		<div class="grow flex flex-row items-center" onpointerdown="document.getElementById('${windowId}').classList.add('dragging');">
+			<img src="/icons/${program.imagedata}" class="h-6 w-6 ml-1">
+			<p class="p-1 select-none">${program.windowname}</p>
 		</div>
-		<div class="grow">
-			<iframe id="${windowId}iframe" src="${url}" class="border-none outline-none origin-top-left"
-				style="width: ${zoomPrc}; height: ${zoomPrc}; transform: scale(${program.zoom});"
-			></iframe>
+		<div class="flex justify-center items-stretch h-full">
+			<!-- Reload iframe -->
+			<div class="hover:bg-white hover:bg-opacity-10 cursor-pointer flex items-center"
+				onclick="document.getElementById('${windowId}iframe').src = document.getElementById('${windowId}iframe').src"
+			>
+				<svg width="50" height="20">
+					<path d="M20,5 18,5 18,15 32,15 32,5 25,5 27,3 27,7 25,5" fill="none" stroke="white" stroke-linejoin="round" stroke-linecap="round" stroke-width="2"></path>
+				</svg>
+			</div>
+			<!-- Fullscreen -->
+			<div class="hover:bg-white hover:bg-opacity-10 cursor-pointer flex items-center"
+				onclick="document.getElementById('${windowId}iframe').requestFullscreen();"
+			>
+				<svg width="50" height="20">
+					<path d="M29,5 32,5 32,8 M32,12 32,15 29,15 M21,5 18,5 18,8 M18,12 18,15 21,15" fill="none" stroke="white" stroke-linejoin="round" stroke-linecap="round" stroke-width="2"></path>
+				</svg>
+			</div>
+			<!-- Minimize window -->
+			<div class="hover:bg-white hover:bg-opacity-10 cursor-pointer flex items-center"
+				onclick="document.getElementById('${windowId}').classList.add('minimized')";
+			>
+				<svg width="50" height="20">
+					<path d="M20,10 30,10" fill="none" stroke="white" stroke-linejoin="round" stroke-linecap="round" stroke-width="2"></path>
+				</svg>
+			</div>
+			<!-- Maximize window -->
+			<div class="hover:bg-white hover:bg-opacity-10 cursor-pointer flex items-center"
+				onclick="document.getElementById('${windowId}').classList.toggle('maximized')";
+			>
+				<svg width="50" height="20">
+					<path d="M18,5 32,5 32,15 18,15 18,5" fill="none" stroke="white" stroke-linejoin="round" stroke-linecap="round" stroke-width="2"></path>
+				</svg>
+			</div>
+			<!-- Close window -->
+			<div class="hover:bg-red-500 cursor-pointer flex items-center"
+				onclick="document.getElementById('${windowId}').remove();"
+			>
+				<svg width="50" height="20">
+					<path d="M20,5 30,15 M30,5 20,15 25" fill="none" stroke="white" stroke-linejoin="round" stroke-linecap="round" stroke-width="2"></path>
+				</svg>
+			</div>
 		</div>
 	</div>
+	<div class="grow">
+		<iframe id="${windowId}iframe" src="${url}" class="border-none outline-none origin-top-left"
+			style="width: ${zoomPrc}; height: ${zoomPrc}; transform: scale(${program.zoom});"
+		></iframe>
+	</div>
 	`;
+	windows.appendChild(windowElement);
 	return false;
 }
 
